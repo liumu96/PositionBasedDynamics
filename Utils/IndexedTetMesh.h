c40ae92ba5be6d77c4c40f14a02d774680277c3f
liumu96 <918883717@qq.com>
2024-01-02 10:24:50 +0800

    Indexed Tet Mesh Class

--- Utils/IndexedTetMesh.h
+++ Utils/IndexedTetMesh.h
@@ -8,7 +8,6 @@ namespace Utilities
 {
     class IndexedTetMesh
     {
-    public:
         struct Edge
         {
             std::array<unsigned int, 2> m_vert;
@@ -54,10 +53,27 @@ namespace Utilities
         ~IndexedTetMesh();
 
         void release();
+        void initMesh(const unsigned int nPoints, const unsigned int nEdges, const unsigned int nFaces, const unsigned int nTets);
+        void addTet(const unsigned int *const indices);
+        void addTet(const int *const indices);
 
+        const Faces &getFaces() const { return m_faceIndices; }
+        Faces &getFaces() { return m_faceIndices; }
         const Tets &getTets() const { return m_tetIndices; }
         Tets &getTets() { return m_tetIndices; }
+        Edges &getEdges() { return m_edges; }
+        const Edges &getEdges() const { return m_edges; }
+        const FaceData &getFaceData() const { return m_faces; }
+        const TetData &getTetData() const { return m_tets; }
+        const VerticesTets &getVertexTets() const { return m_verticesTets; }
+        const VerticesFaces &getVertexFaces() const { return m_verticesFaces; }
+        const VerticesEdges &getVertexEdges() const { return m_verticesEdges; }
 
         unsigned int numVertices() const { return m_numPoints; }
+        unsigned int numFaces() const { return (unsigned int)m_faceIndices.size() / 3; }
+        unsigned int numTets() const { return (unsigned int)m_tetIndices.size() / 4; }
+        unsigned int numEdges() const { return (unsigned int)m_edges.size(); }
+
+        void buildNeighbors();
     };
 }
