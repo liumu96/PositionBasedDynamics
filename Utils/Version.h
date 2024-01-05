81f0c29396e0b0f95ad33f756dbaa6119fbd5d91
liumu96 <918883717@qq.com>
2024-01-05 23:34:55 +0800

    Utility functions

--- Utils/Version.h
+++ Utils/Version.h
@@ -4,7 +4,7 @@
 #define STRINGIZE(x) STRINGIZE_HELPER(x)
 #define WARNING(desc) message(__FILE__ "(" STRINGIZE(__LINE__) ") : Warning: " #desc)
 
-#define GIT_SHA1 "4fb0de0f09251a8ed52ed48307a9023a2c5c4330"
+#define GIT_SHA1 "8a7dbf537cd375a2a591d008b4164dd7070ffa52"
 #define GIT_REFSPEC "refs/heads/main"
 #define GIT_LOCAL_STATUS "DIRTY"
 
